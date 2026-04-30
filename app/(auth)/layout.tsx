6132011c027cc6deb89450bebb12d632bd872254
David Salas <899166@unizar.es>
2026-04-30 19:17:50 +0200

    Eliminado el código comentado del logo en las vistas de autenticación para evitar clics fantasma y actualizacion de el fondo sin logos ni reglas.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,19 +9,11 @@ export default function AuthLayout({
         <div
             className="min-h-screen flex flex-col items-center justify-center relative overflow-hidden"
             style={{
-                backgroundImage: "url('/bg_inicio.png')",
+                backgroundImage: "url('/fondo_menu.png')",
                 backgroundSize: 'cover',
                 backgroundPosition: 'center'
             }}
         >
-            {/* 
-            Zona interactiva del Logo (dibujado en el fondo)
-            <Link 
-                href="/"
-                className="absolute top-4 left-4 w-[12rem] h-[10rem] cursor-pointer z-20"
-                aria-label="Volver al inicio"
-            />
-            */}
 
             <div className="z-10 relative">
                 {children}
